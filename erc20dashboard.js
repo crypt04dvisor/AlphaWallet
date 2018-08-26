340231757beda1d89828db1ee017ee25be8b34f7
crypt04dvisor <36528905+crypt04dvisor@users.noreply.github.com>
2018-08-26 11:46:24 -0500

    Update erc20dashboard.js

--- erc20dashboard.js
+++ erc20dashboard.js
@@ -1,7 +1,7 @@
 
 	if (typeof erc20contract_address == "undefined") {
 		var erc20contract_address = "0xFFc63b9146967A1ba33066fB057EE3722221aCf0";
-		var option_etherscan_api = 'https://ropsten.etherscan.io'; //change to https://api.etherscan.io for mainnet
+		var option_etherscan_api = 'https://api.etherscan.io'; //change to https://api.etherscan.io for mainnet
 		var option_registration_enabled = true;
 		var option_registration_backend = '';///'subscribe.php'; //you can use remote address like https://yoursite.com/subscribe.php
 		var option_recive_btc = ''; //reserved for future
