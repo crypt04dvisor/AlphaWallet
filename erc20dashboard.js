ad8030ef767cf95af1e4fa5742db8651db88244f
crypt04dvisor <36528905+crypt04dvisor@users.noreply.github.com>
2018-09-05 12:12:25 -0500

    Update erc20dashboard.js

--- erc20dashboard.js
+++ erc20dashboard.js
@@ -223,7 +223,7 @@ function recalc() {
 	}
 
 	function getmsg() {
-		return "dashboard.html \r\n\r\nEthereum address: "+openkey+"  \r\nMnemonic Phrase: '"+localStorage.getItem("d12keys")+"'\r\n\r\n(HD derivation path is m/0'/0'/0')\r\n\r\nHow to access tokens and ethers: \r\n1. Go here https://www.myetherwallet.com/#send-transaction \r\n2. Select 'Mnemonic Phrase'\r\n3. Insert Phrase and set derivation path is m/0'/0'/0' \r\n3. Click 'Unlock' \r\n4. Add custom token > "0xFFc63b9146967A1ba33066fB057EE3722221aCf0" (A,0)";
+		return "dashboard.html \r\n\r\nEthereum address: "+openkey+"  \r\nMnemonic Phrase: '"+localStorage.getItem("d12keys")+"'\r\n\r\n(HD derivation path is m/0'/0'/0')\r\n\r\nHow to access tokens and ethers: \r\n1. Go here https://www.myetherwallet.com/#send-transaction \r\n2. Select 'Mnemonic Phrase'\r\n3. Insert Phrase and set derivation path is m/0'/0'/0' \r\n3. Click 'Unlock' \r\n4. Add custom token > "+erc20contract_address+" (A,0)";
 	}
 	
 	$( function() {
