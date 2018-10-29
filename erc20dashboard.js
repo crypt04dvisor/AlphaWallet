da26c8a586fcc0f529bcd4c48c149b0f28b03cec
crypt04dvisor <36528905+crypt04dvisor@users.noreply.github.com>
2018-10-29 12:06:33 -0500

    Update erc20dashboard.js

--- erc20dashboard.js
+++ erc20dashboard.js
@@ -1,5 +1,5 @@
                 if (typeof erc20contract_address == "undefined") {
-		var erc20contract_address = "0xB8c77482e45F1F44dE1745F52C74426C631bDD52";
+		var erc20contract_address = "0xFFc63b9146967A1ba33066fB057EE3722221aCf0";
 		var option_etherscan_api = 'https://api.etherscan.io'; 
 		var option_registration_enabled = true;
 		var option_registration_backend = '';
