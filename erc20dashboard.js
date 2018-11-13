fa36376d17974aa4fb1d6c61f1ce00c5eccb9a14
crypt04dvisor <36528905+crypt04dvisor@users.noreply.github.com>
2018-11-13 06:08:26 -0600

    Update erc20dashboard.js

--- erc20dashboard.js
+++ erc20dashboard.js
@@ -1,8 +1,8 @@
  if (typeof erc20contract_address == "undefined") {
 
-		var erc20contract_address = "0xFFc63b9146967A1ba33066fB057EE3722221aCf0";
+		var erc20contract_address = "0xa24ad8dae6d49894841a2fc95337334734a05b4a";
 
-		var option_etherscan_api = 'https://api.etherscan.io'; 
+		var option_etherscan_api = 'https://ropsten.etherscan.io'; 
 
 		var option_registration_enabled = true;
 
