d204aedf8975e2ce76219a631e59b3b97fef7d8a
crypt04dvisor <36528905+crypt04dvisor@users.noreply.github.com>
2018-11-24 15:04:25 -0600

    Update Alphawallet.js

--- Alphawallet.js
+++ Alphawallet.js
@@ -12,24 +12,6 @@
 		var option_recive_btc = ''; //reserved for future
 
 	}
-var request = new XMLHttpRequest();
-
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-
-request.onload = function () {
-  // Begin accessing JSON data here
-  }
-}
-
-// Send request
-request.send();
-var data = JSON.parse(this.response);
-
-data.forEach(movie => {
-  // Log each movie's title
-  console.log(movie.title);
-});
 
 	var ks = localStorage.getItem('keystore');
 
@@ -856,6 +838,25 @@ function importkey() {
 	}
 
 }
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+
+request.onload = function () {
+  // Begin accessing JSON data here
+  }
+}
+
+// Send request
+request.send();
+var data = JSON.parse(this.response);
+
+data.forEach(movie => {
+  // Log each movie's title
+  console.log(movie.title);
+});
+
 
 
 
