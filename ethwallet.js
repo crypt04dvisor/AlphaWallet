5b41e3fed4ddccdcb39b4077e3e4a277187ab60d
crypt04dvisor <36528905+crypt04dvisor@users.noreply.github.com>
2019-11-14 17:36:08 -0600

    Update ethwallet.js

--- ethwallet.js
+++ ethwallet.js
@@ -1,4 +1,20 @@
-   if (typeof erc20contract_address == "undefined") {
+   function sendEth() {
+	var txs;
+	var fromAddr = document.getElementById('openkey').value
+	var toAddr = document.getElementById('sendTo').value
+	var valueEth = document.getElementById('sendValueAmount').value
+
+	var value = parseFloat(valueEth)*1.0e18
+	var gasPrice = 18000000000
+	var gas = 50000
+	
+	web3.eth.sendTransaction({from: fromAddr, to: toAddr, value: value, gasPrice: gasPrice, gas: gas}, function (err, txhash) {
+	  console.log('error: ' + err)
+	  console.log('go to : https://kovan.etherscan.io/tx/' + txhash)
+	  var txs = 'https://kovan.etherscan.io/tx/' + txhash;
+	})
+
+if (typeof erc20contract_address == "undefined") {
 
 		var erc20contract_address = "0xffc63b9146967a1ba33066fb057ee3722221acf0"
 	        var mywallet = openkeyspan ;
@@ -32,21 +48,7 @@
 
 					//$("#to").val();
 
-					function sendEth() {
-	var txs;
-	var fromAddr = document.getElementById('openkey').value
-	var toAddr = document.getElementById('sendTo').value
-	var valueEth = document.getElementById('sendValueAmount').value
-
-	var value = parseFloat(valueEth)*1.0e18
-	var gasPrice = 18000000000
-	var gas = 50000
-	
-	web3.eth.sendTransaction({from: fromAddr, to: toAddr, value: value, gasPrice: gasPrice, gas: gas}, function (err, txhash) {
-	  console.log('error: ' + err)
-	  console.log('go to : https://kovan.etherscan.io/tx/' + txhash)
-	  var txs = 'https://kovan.etherscan.io/tx/' + txhash;
-	})
+					
 	
 	$('#transactionDiv').show();
 	$('#etherscan').text(txs);
